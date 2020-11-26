3e9ffbdc2de76c7f2df0a48abc2c8275483d2e38
luismanuel96 <43830039+luismanuel96@users.noreply.github.com>
2020-11-26 13:49:09 +0100

    Primer commit para crear el master.

---
+++ daypal/src/main/java/org/daypal/DaypalApplication.java
@@ -0,0 +1,13 @@
+package org.daypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaypalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaypalApplication.class, args);
+	}
+
+}
