4e61e5e0d445a20186d2526c54ef133769d64f71
luismanuel96 <43830039+luismanuel96@users.noreply.github.com>
2021-04-22 20:24:08 +0200

    Arreglo a la url para el getMeals de admin.

--- src/main/java/com/bezkoder/spring/security/postgresql/security/WebSecurityConfig.java
+++ src/main/java/com/bezkoder/spring/security/postgresql/security/WebSecurityConfig.java
@@ -59,7 +59,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
 			.antMatchers("/api/test/**").permitAll()
-			.antMatchers("/meals/all").hasAuthority("ROLE_ADMIN")
+			.antMatchers("/meals").hasAuthority("ROLE_ADMIN")
 			.anyRequest().authenticated();
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
